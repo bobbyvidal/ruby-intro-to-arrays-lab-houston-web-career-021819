354b1c57a0b1c44ced9aa76e5835c2b554cc4361
Robert Vidal <bobbyvidal@gmail.com>
2019-02-20 17:48:20 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,17 @@
+def instantiate_new_array 
+  empty_array = []
+end
+
+def array_with_two_elements 
+  array_2 = ["thing1", "thing2"]
+  return array_2
+end
+def first_element(array)
+  array[0]
+end
+def third_element(array)
+  array[2]
+end
+def last_element(array)
+  array[-1]
+end
